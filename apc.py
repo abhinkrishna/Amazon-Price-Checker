a71001e030aa3b8229422a0625c09bcc796bd575
Abhin Krishna <abhinkrishna.ka@gmail.com>
2019-07-07 00:07:17 +0530

    generelized

--- apc.py
+++ apc.py
@@ -1,9 +1,14 @@
 import requests
 import bs4
 import smtplib
-
-url = 'https://www.amazon.in/JBL-C100SI-Ear-Headphones-Black/dp/B01DEWVZ2C/ref=sr_1_3?crid=168N7S8O12VZ3&keywords=jbl+c100si+earphone+with+mic&qid=1561688806&s=gateway&sprefix=jbl+%2Caps%2C405&sr=8-3'
-
+# create a myAccount.py file on same directory with content below
+    ####################################################
+    # username = <yourMailAddress@serviceProvider.com> #
+    # password = <yourAccountPassword>                 #
+    ####################################################
+import myAccount # importing myAccount.py file.
+url = input("Product URL : ")
+des_price = int(input("Desired price : "))
 headers = {
     "User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 }
@@ -12,34 +17,43 @@ def check_price():
     res = requests.get(url)
     webContent = res.text
     soup = bs4.BeautifulSoup(webContent,'lxml')
-    item = soup.find(id="productTitle").get_text()
+    item = soup.find(id="productTitle").getText()
     item = item.strip()
-    price = soup.find(id="priceblock_ourprice").get_text()
+    price = soup.find(id="priceblock_ourprice").getText()
     price = str(price.encode('utf8'))
     price = price[22:]
     price = float(price[:6])
     print('Item :' , item)
     print('Price :' , price)
-    if(price < 600.0):
-        send_mail()
+    if(price < des_price):
+        print("Desired price is less than" , price)
+        res = input("Do you want to mail information (y/n)"))
+        if(res == 'y' || res == 'Y'):
+            send_mail()
+        else:
+            print('Okay... email service ignored')
 
 def send_mail():
-    print("initializing mail server...")
+    to_mailaddress = input("e-mail address of recipient : ")
+    print("Initializing mail services")
+    print("Connection establishing....")
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('info.hark.in@gmail.com','prxgvibaoqfydwxu')
-    subject = 'Price droped down (Test)'
-    body = 'Link : https://www.amazon.in/JBL-C100SI-Ear-Headphones-Black/dp/B01DEWVZ2C/ref=sr_1_3?crid=168N7S8O12VZ3&keywords=jbl+c100si+earphone+with+mic&qid=1561688806&s=gateway&sprefix=jbl+%2Caps%2C405&sr=8-3'
-
+    server.login(myAccount.username,myAccount.password)
+    print("Connection established")
+    print("writing message...")
+    subject = 'Test Email from python'
+    body = 'Hi, this is a test run...'
     message  = """ Subject: %s \n\n %s """ % (subject , body)
+    print("Sending e-mail to ",to_mailaddress)
     server.sendmail(
-        'info.hark.in@gmail.com',
-        'abhinkrishna.ka@gmail.com',
+        myAccount.username,
+        to_mailaddress,
         message
     )
-    print('Email has been send...')
+    print('Sucess!')
     server.quit()
 
 check_price()
